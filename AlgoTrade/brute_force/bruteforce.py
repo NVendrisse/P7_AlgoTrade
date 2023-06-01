2ba5217325a2cf3ed346aeccd7861b7194fe4e0f
NVendrisse <nicolas@vendrisse.com>
2023-06-02 00:42:15 +0200

    bigO notation

--- AlgoTrade/brute_force/bruteforce.py
+++ AlgoTrade/brute_force/bruteforce.py
@@ -21,17 +21,19 @@ dataset = open(str(sys.argv[1]), mode="r", encoding="UTF-8")
 data = csv.reader(dataset)
 table_data = []
 next(data, None)
+
+#O(n)
 for row in data:
     table_data.append(Action(row[0], float(row[1]), float(row[2])))
 totaux=[]
+
+#O(2n) ou O(n**3)
 for i in range(2,len(table_data)):
     table=combinations(table_data,i)
-    
     for e in table:
         tot=0
         tot_gain=0
         acts=[]
-        
         for j in e:
             acts.append(j.name)
             tot+=j.cost
@@ -39,6 +41,7 @@ for i in range(2,len(table_data)):
     totaux.append((acts,tot,tot_gain))
 
 best=totaux[0]
+#O(n)
 for ac in totaux:
     if ac[1]<=500:
         if ac[2]>best[2]:
